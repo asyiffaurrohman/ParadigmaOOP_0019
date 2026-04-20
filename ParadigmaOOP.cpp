b70b560f86e74b7c4c1bff9e3b5e1515bd03dfa5
asyiffaurrohman <akungithub934@gmail.com>
2026-04-20 10:33:37 +0700

    menambahkan isi metode

--- ParadigmaOOP.cpp
+++ ParadigmaOOP.cpp
@@ -10,5 +10,12 @@ public :
     float nilai;
 
     void printData(){
-       
+        cout << "NIM : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
+
+    }
+}; //batas class
+
+
 
